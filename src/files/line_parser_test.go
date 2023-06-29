3b227bab59845c0244fab501b4dd42534d6b4a6c
Przemysław Białoń <przem0.b95@gmail.com>
2023-06-29 02:46:58 +0200

    added test for line parser

---
+++ src/files/line_parser_test.go
@@ -0,0 +1,46 @@
+package files
+
+import "testing"
+
+func Test_logLineImpl_Timestamp(t *testing.T) {
+	type fields struct {
+		line string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    int64
+		wantErr bool
+	}{
+		{
+			name: "should parse datetime",
+			fields: fields{
+				line: "2021-01-01T00:00:00Z,log1 line1",
+			},
+			want:    1609459200,
+			wantErr: false,
+		}, {
+			name: "should return error if datetime is invalid",
+			fields: fields{
+				line: "2021-13-01T00:00:00,log1 line1",
+			},
+			want:    0,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &logLineImpl{
+				line: tt.fields.line,
+			}
+			got, err := l.Timestamp()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Timestamp() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Timestamp() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
