c11fe40236adb6bd92d97c88960a2addac4d4979
Przemysław Białoń <przem0.b95@gmail.com>
2023-06-29 02:44:57 +0200

    prepared line parser

---
+++ src/files/line_parser.go
@@ -0,0 +1,35 @@
+package files
+
+import (
+	"strings"
+	"time"
+)
+
+const iso8601Layout = "2006-01-02T15:04:05Z"
+
+type LogLine interface {
+	timestamp() (int64, error)
+	raw() string
+}
+
+type logLineImpl struct {
+	line string
+}
+
+func (l *logLineImpl) Timestamp() (int64, error) {
+	logLineParts := strings.Split(l.line, ",")
+	datetime := logLineParts[0]
+	return l.parseDatetime(datetime)
+}
+
+func (l *logLineImpl) Raw() string {
+	return l.line
+}
+
+func (l *logLineImpl) parseDatetime(datetime string) (int64, error) {
+	dateTime, err := time.Parse(iso8601Layout, datetime)
+	if err != nil {
+		return 0, err
+	}
+	return dateTime.Unix(), nil
+}
